24b040799106ae3eee6536649036e5b860b99d1b
tuannhat24 <tuannhat124@gmail.com>
2024-06-03 20:02:46 +0700

    animation pencil

---
+++ src/components/penanimation/PenAnimation.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import styles from "./PenAnimation.module.css";
+
+const PenAnimation = () => {
+  return (
+    <div className={styles.paper}>
+      <div className={styles.penAnimation}>
+        <img src="/paper.png" alt="Paper" className={styles.paper} />
+        <img
+          src="/pencil-solid.svg"
+          alt="Pen Icon"
+          className={styles.penIcon}
+        />
+          <div className={styles.textOverlay}>Create a blog in your own way.</div>
+      </div>
+    </div>
+  );
+};
+
+export default PenAnimation;
